8c4eefd86257d9f1b0278d769ea7abc4689aa91a
jerry <jerry@jerryui-MacBookPro.local>
2021-09-04 19:10:22 +0900

    Update eslint & prettier

--- App.tsx
+++ App.tsx
@@ -4,7 +4,6 @@ import React, { ReactElement, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { Camera } from "expo-camera";
-import { render } from "react-dom";
 
 type posType = {
   x: number;
